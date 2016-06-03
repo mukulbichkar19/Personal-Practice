868e9770afe0dd8ef71bc8b68958bc74b7ff8135
Mukul Bichkar <Mukul Bichkar>
2016-06-03 18:31:37 -0400

    Done with madlibs

--- 30DaysOfCode/MADLIB.java
+++ 30DaysOfCode/MADLIB.java
@@ -86,7 +86,14 @@ public class MADLIB{
 	}
 
 	public void setRandomNums(){
-		
+		int num = Math.abs(rand.nextInt()) % 100;
+		int index = 0;
+		int[] numberHolder = new int[3];
+		while(index < numberHolder.length){
+			numberHolder[index] = num + index;
+			index++;
+		}
+		randNums = "not "+numberHolder[0]+", not "+numberHolder[1]+", but "+numberHolder[2];
 	}
 
 	public void printInstruction(){
@@ -95,13 +102,28 @@ public class MADLIB{
 	}
 
 	public void putTogether(){
-		String storybody = "Jessie and her friend "+getName()+" went to DisneyLand today. "
+		String storybody;
+		int num = Math.abs(rand.nextInt())%2;
+		if(num==0){
+		
+		storybody = "Jessie and her friend "+getName()+" went to DisneyLand today. "
 		+" They saw a "+getNoun1()+" at Magic Kingdom"
 		+" and ate "+getAdjective1()+" feast for dinner. The next day I"
 		+" ran "+getAdverb()+" to see mickey mouse in his"+getNoun2()+" "
 		+" and then at the night I gazed at the "+getRandomNums()+" "+getAdjective2()+" fireworks shooting from"
 		+getNoun3()+".";
+		
+		} else {
+			storybody = "Amanda and Aishwarya "+getName()+" went to the zoo last summer. "+
+						"They saw a huge "+getNoun1()+" and a tiny little "+getNoun2()+". That night "
+						+ "they decided to climb "+getAdverb()+" into the "+getNoun3()+" to get a closer look."
+						+ "The zoo was "+getAdjective2()+"at night ,but they did not care until"+getRandomNums()+" "
+						+getAdjective2()+" apes yelled in their faces, making"
+						 +"Amanda and "+getName()+" sprint all the way back home";
+
+		}
 		setStory(storybody);
+
 	}
 
 	public void enterName(){
@@ -109,27 +131,49 @@ public class MADLIB{
 	}
 
 	public void enterNoun1(){
+		System.out.println("Give me a noun: ");
 		setNoun1(scanner.nextLine());
 	}
 
 	public void enterNoun2(){
+		System.out.println("Give me a noun: ");
 		setNoun2(scanner.nextLine());
 	}
 
 	public void enterNoun3(){
+		System.out.println("Give me a noun: ");
 		setNoun3(scanner.nextLine());
 	}
 
 	public void enterAdjective1(){
+		System.out.println("Give me an adjective: ");
 		setAdjective1(scanner.nextLine());
 	}
 
 	public void enterAdjective2(){
+		System.out.println("Give me an adjective: ");
 		setAdjective2(scanner.nextLine());
 	}
 
 	public void enterAdverb(){
+		System.out.println("Give me an adverb: ");
 		setAdverb(scanner.nextLine());
+	}
+
+	public void play(){
+		
+		enterName();
+		enterNoun1();
+		enterAdjective1();
+		enterAdverb();
+		enterNoun2();
+		enterAdjective2();
+		enterNoun3();
+		setRandomNums();
+		putTogether();
+		System.out.println(getStory());
+
+
 	}
 
 
@@ -142,6 +186,7 @@ public class MADLIB{
 
 		MADLIB game = new MADLIB();
 		game.printInstruction();
+		game.play();
 
 	}
 
