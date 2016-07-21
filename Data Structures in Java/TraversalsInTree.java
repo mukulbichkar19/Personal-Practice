80fd5c280d350600525a1bc6e4e4e0e902971500
mukulbichkar19 <mukulbichkar@hotmail.com>
2016-07-21 18:27:43 -0400

    Tree level traversal

--- Data Structures in Java/TraversalsInTree.java
+++ Data Structures in Java/TraversalsInTree.java
@@ -159,6 +159,39 @@ public ArrayList<Integer> postOrderIterative(BinaryTreeNode root){
 
 }	
 
+// Level Order Traversal
+public ArrayList<ArrayList<Integer>> levelOrderIterative(BinaryTreeNode root){
+	ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+	if(root == null){
+		return res;
+	}
+	Queue<BinaryTreeNode> q = new LinkedList<BinaryTreeNode>();
+	q.offer(root);
+	q.offer(null);
+
+	ArrayList<Integer> curr = new ArrayList<Integer>();
+	while(!q.isEmpty()){
+		BinaryTreeNode tmp = q.poll();
+		if(tmp!=null){
+			curr.add(tmp.data);
+			if(tmp.left != null){
+				q.offer(tmp.left);
+			}
+			if(tmp.right != null){
+				q.offer(tmp.right);
+			}
+		}else{
+			ArrayList<Integer> c_curr = new ArrayList<Integer>(curr);
+			res.add(c_curr);
+			curr.clear();
+			if(!q.isEmpty()){
+				q.offer(null);
+			}
+		}
+	}
+	return res;
+}
+
 
 	
 
@@ -226,6 +259,17 @@ public ArrayList<Integer> postOrderIterative(BinaryTreeNode root){
 			System.out.print(s+" ");
 		}
 		System.out.println ();
+
+		System.out.println("Level Order Traversal (Iteratively): ");
+		ArrayList<ArrayList<Integer>> res3 = tree.levelOrderIterative(root);
+		for(ArrayList<Integer> s:res3){
+			for(Integer p:s)
+			System.out.print(p+" ");
+		}
+		System.out.println ();
+
+
+
 
 
 
