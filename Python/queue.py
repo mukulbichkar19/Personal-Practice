d9bf505d3d39e5edf20442e23efe7965ea9c5bce
mukulbichkar19 <mukulbichkar@hotmail.com>
2016-08-25 23:05:30 -0400

    Adding binary search and queue

---
+++ Python/queue.py
@@ -0,0 +1,22 @@
+""" Queue in python """
+from collections import deque
+
+class Queue:
+    def __init__(self,head=None):
+        self.storage = [head]
+        
+    def enqueue(self,new_element):
+        self.storage.append(new_element)
+    
+    def peek(self):
+        return self.storage[0]
+
+    def dequeue(self):
+        return self.storage[0]
+    
+      
+
+q = Queue(1)
+q.enqueue(2)
+q.enqueue(3)
+print q.peek()
