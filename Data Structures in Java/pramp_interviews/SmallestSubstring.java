748ef7e81583ba95a9bfe2073a97611cf6c99df6
mukulbichkar19 <mukulbichkar@hotmail.com>
2016-10-16 13:45:47 -0400

    Smallest substring brute force

--- Data Structures in Java/pramp_interviews/SmallestSubstring.java
+++ Data Structures in Java/pramp_interviews/SmallestSubstring.java
@@ -2,16 +2,36 @@ public class SmallestSubstring{
 
   public static String checksmallestsubstring(String[] arr, String tobe_matched){
 
+    String match = null;
     // Approach 1: brute force
-    System.out.println(tobe_matched);
+    int arr_len = arr.length;
+    // Find hex values of string arr
+    int arr_hex = 0;
     for(String s:arr){
-      System.out.print(s+" ");
+       arr_hex ^= s.charAt(0);
+    }
+    System.out.println(arr_hex);
+    for(int i=0;i<tobe_matched.length()-arr_len+1;){
+        String substring = tobe_matched.substring(i,i+arr_len);
+        if(findHex(substring)==arr_hex)
+        {
+          match = substring;
+          break;
+        }
+        i++;
     }
 
-    return tobe_matched;
+    return match;
 
   }
 
+  private static int findHex(String s){
+    int hex_value = 0;
+    for(int i=0;i<s.length();i++){
+      hex_value ^= s.charAt(i);
+    }
+    return hex_value;
+  }
 
   public static void main(String args[]){
 
