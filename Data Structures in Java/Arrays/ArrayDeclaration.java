b9911ee3e0976b9d8adb63d90787d9135b5085e9
mukulbichkar19 <mukulbichkar@hotmail.com>
2017-02-07 13:26:59 -0500

    After inserting Makefile and cleaning the class files

---
+++ Data Structures in Java/Arrays/ArrayDeclaration.java
@@ -0,0 +1,22 @@
+public class ArrayDeclaration{
+
+	public static void main(String args[]){
+
+		// Declare a constant for size
+		int array_size = 10;
+	
+		//Array Declaration in Java for Interviews
+		
+		// Way 1 Preferred Method
+		int[] myArray = new int[array_size];
+
+		// Way 2
+		int[] myArray2 = {0,1,2,3,4,5,6,7,8,9};
+
+		// Way 3
+		int myArray3[] = new int[array_size];
+
+	}
+}
+
+
