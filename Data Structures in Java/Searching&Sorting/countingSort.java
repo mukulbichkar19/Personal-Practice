2e17431023bd4984973eb08c7a891f8ca53291f6
Mukul Bichkar Personal <mukulbichkarpersonal@MacBook-Pro.local>
2017-07-01 20:23:19 -0400

    counting sort

--- Data Structures in Java/Searching&Sorting/countingSort.java
+++ Data Structures in Java/Searching&Sorting/countingSort.java
@@ -1,64 +1,40 @@
 public class countingSort{
-	
-
-	void counting(int[] arr){
-
-		int length = arr.length;
-
-		int max = 0;
-		// Find the range
-		for(int i=0;i<length;i++){
-			if(arr[i] > max){
-				max = arr[i];
-			}
-		}
-		//System.out.println("The lower end of range is "+max);
-		int[] count_arr = new int[max+1];
 
 
-		// Store the count of each number
-		for(int i=0;i<length;i++){
-			count_arr[arr[i]]=count_arr[arr[i]]+1;
-		}
+  private static int[] countingSort(int[] a, int[] b, int k){
+    int[] c = new int[k+1];
 
-		// Add the previous elements in the array
-		for(int i=1;i<max+1;i++){
-			count_arr[i] += count_arr[i-1];
-		}
+    for(int j=0;j<a.length;j++){
+      c[a[j]] = c[a[j]] + 1;
+    }
 
-		int[] output = new int[length];
+    for(int i=1;i<c.length;i++){
+      c[i] += c[i-1];
+    }
 
-		for(int i=0;i<length;i++){
-			output[count_arr[arr[i]]-1] = arr[i];
-			count_arr[arr[i]] = count_arr[arr[i]]-1;
-		}
+    for(int j=a.length-1;j>=0;j--){
+      b[c[a[j]]] = a[j];
+      c[a[j]] = c[a[j]]-1;
+    }
+    return b;
 
-		System.out.println();
-		System.out.println("The sorted array is: ");
-		for(int i=0;i<length;i++){
-			System.out.print(output[i]+" ");
-		}
+  }
 
 
-
-	}
-
-
-
-	public static void main(String args[]){
-
-
-		int[] arr = {1,4,1,2,7,5,2,9};
-		System.out.println("The array before sorting is: ");
-		for(int i=0;i<arr.length;i++){
-			System.out.print(arr[i]+" ");
-		}
-
-		countingSort s = new countingSort();
-		s.counting(arr);
+  public static void main(String args[]){
+    int[] a = {2,5,3,0,2,3,0,3};
+    int[] b = new int[a.length+1];
+    java.util.Scanner scan = new java.util.Scanner(System.in);
+    System.out.println("Enter the value of k: ");
+    int k = scan.nextInt();
+    b = countingSort(a, b, k);
+    System.out.println("Sorted Array: ");
+    for(int i=0;i<b.length;i++){
+      System.out.print(b[i] + " ");
+    }
+    System.out.println();
 
 
+  }
 
-
-	}
 }
