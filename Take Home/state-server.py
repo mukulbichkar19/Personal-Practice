cf99888e9d398de2782bfebf2e8ec734d68bff70
mukul1992 <mukul1992@ccs.neu.edu>
2018-03-04 18:35:52 -0800

    Final commit

--- Take Home/state-server.py
+++ Take Home/state-server.py
@@ -37,29 +37,24 @@ def areCollinear(point, line):
 	bx,by = b
 	cx,cy = point
 
-	# (a), (b), and (c) are collinear (all on same line) if the cross-product of (b-a) and (c-a) is 0.
 	crossProduct = (cy - ay) * (bx - ax) - (cx - ax) * (by - ay)
 	if abs(crossProduct) <= float_info.epsilon:
-		# Check if (c) is between (a) and (b)
 		if min(ax, bx) <= cx <= max(ax, bx) and min(ay, by) <= cy <= max(ay, by):
 			return True
 
 	return False
 
-# Check if point is in polygon or on one of its edges/vertices.
+
 def isInsidePOlygon(point, polygon):
-	#Check if point lies on the polygon
 	if point in polygon:
 		return True
 
-	# Loop through all edges of the polygon. Each edge is represented as a line segment between point (p1) and point (p2).
 	inside = False
 	numVertices = len(polygon)
 	for i in range(numVertices-1):
 		p1 = polygon[i]
 		p2 = polygon[i+1]
 
-		# Return True if point is on edge of polygon.
 		edge = [p1, p2]
 		if areCollinear(point, edge):
 			return True
@@ -67,17 +62,12 @@ def isInsidePOlygon(point, polygon):
 		x,y = point
 		p1x,p1y = p1
 		p2x,p2y = p2
-		# Check to see if ray is within horizontal bounds.
+
 		if y > min(p1y,p2y) and y <= max(p1y,p2y):
-			# Make sure ray starts before maximum vertical bound
 			if x <= max(p1x,p2x):
-				# Make sure edge isn't horizontal
 				if p1y != p2y:
-					# Check where ray intersects with edge.
 					xIntersect = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
-					# If edge is vertical (and ray starts before it, as checked above) or ray starts before where it intersects with the edge (xIntersect), then the ray has crossed an edge.
 					if p1x == p2x or x <= xIntersect:
-						# odd number means lies inside, even number means lies outside
 						inside = not inside
 	return inside
 
