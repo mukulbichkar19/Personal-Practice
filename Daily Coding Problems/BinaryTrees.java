8a7f85af9d69c22c749496fe72a0a246fe724af7
mukulbichkar19 <mukulbichkar@hotmail.com>
2018-06-04 16:32:56 -0400

    Added input file

--- Daily Coding Problems/BinaryTrees.java
+++ Daily Coding Problems/BinaryTrees.java
@@ -1,28 +1,12 @@
 public class BinaryTrees{
 
-
-
-
-
   private static int noTrees(int n){
-
       int[] arr = new int[n];
-
       int count = 0;
       for(int i=0;i<arr.length;i++){
         arr[i] = i+1;
       }
-
-      for(int i=0;i<arr.length;i++){
-          int left_count = findCount(arr, 0, i-1);
-          int right_count = findCount(arr, i+1, arr.length-1);
-          if (left_count > 0 && right_count > 0){
-            count += (left_count*right_count);
-          }else{
-              count += Math.max(left_count, right_count);
-          }
-      }
-      return count;
+      return findCount(arr, 0, arr.length-1);
   }
 
   private static int findCount(int[] arr, int st, int end){
@@ -34,6 +18,7 @@ public class BinaryTrees{
     if(st == end){
       return 1;
     }
+
     int count = 0;
 
     for(int i=st;i<=end;i++){
